bc5e950b07f251e922c74390ee2ff632c7a4b869
Natan <barnyard22993@gmail.com>
2025-09-25 19:39:19 +0300

    Finished Challenge0

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Challenges/Chalange0.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.Challenges;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class Chalange0 extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
