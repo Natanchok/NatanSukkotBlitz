68fa30a9a44b55b92d5636a4136491f518e26a6a
Natanchok <natanmarkch@gmail.com>
2025-09-30 19:41:23 +0300

    added a function that prints hello world

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Challenges/Chalange0.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Challenges/Chalange0.java
@@ -6,7 +6,13 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp
 public class Chalange0 extends LinearOpMode {
     @Override
+    String name = "Chalange0";
+
     public void runOpMode() throws InterruptedException {
         telemetry.addData("you did a thing", "yay");
+    }
+
+    public static void main(String[] args)  {
+        System.out.println("hello world");
     }
 }
