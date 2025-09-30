ebb2e1b4e87fbb616ad59efc3f949e51eccd3434
Natanchok <natanmarkch@gmail.com>
2025-09-30 20:06:04 +0300

    added a class named bankaccount did there some functiona and now im a very cool guy

--- BasicJava/src/main/java/com/example/basicjava/MyClass.java
+++ BasicJava/src/main/java/com/example/basicjava/MyClass.java
@@ -3,6 +3,13 @@ package com.example.basicjava;
 public class MyClass {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Challange11 account1 = new Challange11("Natan haham", 1000.0);
 
+        account1.deposit(500.0);
+        account1.withdraw(200.0);
+
+        System.out.println("Account balance: " + account1.ballance);
+        System.out.println("Account owner: " + account1.ownerName);
+        System.out.println("Account balance: " + account1.getBalance());
     }
 }
