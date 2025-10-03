5a462da9449a02335f5658bba80b5fb145cb59cc
Barnyard22993 <barnyard22993@gmail.com>
2025-10-03 15:55:00 +0300

    created a Teleop called Drivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Challenges/Drivetrain.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.Challenges;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+
+public class Drivetrain extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+
+    }
+}
